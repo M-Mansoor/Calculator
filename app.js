d998038b4d699e952956b37089f2868b1159bb23
mansoor <asadmansoor11120@gmail.com>
2020-06-27 23:46:30 +0500

    Calculator

---
+++ app.js
@@ -0,0 +1,19 @@
+function getNum(num){
+   var result = document.getElementById("result");
+   result.value += num;
+}
+
+function clearResult(){
+     var result = document.getElementById("result");
+  result.value = "";
+}
+
+function showZero(){
+     var result = document.getElementById("result");
+  result.value = "0";
+}
+
+function getResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value);
+}
